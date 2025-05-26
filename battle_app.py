6bf0d5bf03dd11e8523a001300eee706fcf4a93b
Lorenzo Vittori <146745420+lorenzvittori@users.noreply.github.com>
2025-05-26 06:44:39 +0200

    Update battle_app.py

--- battle_app.py
+++ battle_app.py
@@ -123,7 +123,7 @@ if btn:
         st.success(f"⚔️ Optimized result: {result.num} {result.troop.capitalize()}")
 
         st.markdown("### Optimal battle sequence:")
-        armies_str = " ➤ ".join(f"{int(army.num)}({Emoji_Dict[army.troop]})" for army in battle_order.armies)
+        armies_str = "  ↷ ".join(f"{int(army.num)}({Emoji_Dict[army.troop]})" for army in battle_order.armies)
         st.write(armies_str)
         #st.markdown("<span style='color: #1f77b4; font-weight: bold; margin-top:4px;'>🗡️ - Swordsmen</span>", unsafe_allow_html=True)
         #st.markdown(armies_str, unsafe_allow_html=True)
